3db50778f24aa8f4343f67800f962bd2239b5e04
Masoud Soleimani <masoudsol@gmail.com>
2025-03-17 18:13:03 -0400

    Initial Commit

---
+++ Globe News Today/Globe_News_TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Globe_News_TodayApp.swift
+//  Globe News Today
+//
+//  Created by Masoud Soleimani on 2025-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Globe_News_TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
